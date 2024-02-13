2169dd5d6326ebc8f8482fe102d8bd2d23bdb8a3
sandranymark <sandra@nymark.nu>
2024-02-13 17:53:00 +0100

    handled the promise

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 const accessKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzgwZmRkMjExNDBmYjI5YTA5ZTk5NTMxYzMwZDQ0YiIsInN1YiI6IjY1Y2E1MzcxYjZjZmYxMDE4NWE4OGRmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pJWG2rwuCA2FzycBlXtW8HD-_ILid8Hdsc3aAjVzkGw";
-// const apiBaseUrl = "https://api.themoviedb.org/3";
 const apiBaseUrl = `https://api.themoviedb.org/3/trending/all/week?api_key=${accessKey}`
 const moviesContainer = document.getElementById("movies");
 
@@ -16,7 +15,12 @@ async function fetchMovies() {
 
 
     fetch(`${apiBaseUrl}/discover/movie`, options)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
             data.results.forEach(media => {
                 console.log(media);
@@ -24,22 +28,43 @@ async function fetchMovies() {
                 moviesContainer.appendChild(movieCard);
             });
         })
-        .catch(err => console.error(err));
+        .catch(error => {
+            // Handle any errors here
+            console.log('There was an error!', error);
+        });
 }
 
 function createMovieCard(media) {
-    const { title, name, backdrop_path } = media;
+    const { title, name, backdrop_path, id } = media;
 
     const movieCard = document.createElement("div");
     movieCard.classList.add("movie_item")
 
     movieCard.innerHTML = `
     <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}" class="movie_img_rounded">
-    <div class = "title">${title || name}</div>
-    
-    `;
+    <div class="title" data-id="${id}">${title || name}</div>`;
+
     return movieCard;
 
 }
 
 fetchMovies();
+
+moviesContainer.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('title')) {
+        const movieName = target.innerText;
+        console.log('clicked on movie:', movieName);
+    }
+});
+
+moviesContainer.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('title')) {
+        const movieName = target.innerText;
+        console.log('clicked on movie:', movieName);
+
+        // Set the URL of the new page with the movie information
+        window.location.href = `movie-details.html?movie=${encodeURIComponent(movieName)}`;
+    }
+});
